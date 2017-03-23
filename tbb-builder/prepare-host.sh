344909666ac05adc7ba5bace8abf135be17dd09b
lancerajee <lancerajee@gmail.com>
2017-03-23 23:01:16 +0530

    Update prepare-host.sh

--- tbb-builder/prepare-host.sh
+++ tbb-builder/prepare-host.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 sudo apt-get update
 
-# install docker repository key
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv- keys 58118E89F3A912897C070ADBF76221572C52609D
-
-# install and run 'apt-add-repository'
-sudo apt-get install -y software-properties-common python-software-properties
-sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
-sudo apt-get update
-
-# use docker-engine repository as default
-apt-cache policy docker-engine
-
 # install docker
-sudo apt-get install -y docker-engine
+apt-get install -y docker.io
 
 # add root to docker group
 sudo usermod -aG docker root
@@ -22,10 +11,24 @@ sudo usermod -aG docker root
 test $(whoami) = root || sudo usermod -aG docker $(whoami)
 
 # install required packages
-apt-get install -y make libyaml-libyaml-perl libtemplate-perl libio-handle-util-perl libio-all-perl libio-captureoutput-perl libfile-slurp-perl libstring-shellquote-perl libsort-versions-perl libdigest-sha-perl libdata-uuid-perl libdata-dump-perl git
+apt-get install -y make libyaml-libyaml-perl libtemplate-perl libio-handle-util-perl libio-all-perl libio-captureoutput-perl libfile-slurp-perl libstring-shellquote-perl libsort-versions-perl libdigest-sha-perl libdata-uuid-perl libdata-dump-perl libfile-copy-recursive-perl git
 
 mkdir -p /var/src
 cd /var/src
+
+# clone Chromium depot tools 
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+export PATH=$PATH:/var/src/depot_tools
+
+# Fetch Chromium Files
+fetch chromium
+
+#Install Depedencies required to build
+cd src/build
+./install-build-deps.sh
+
+cd ../../
+
 # clone torbrowser repository
 git clone https://git.torproject.org/builders/tor-browser-build.git
 cd tor-browser-build
